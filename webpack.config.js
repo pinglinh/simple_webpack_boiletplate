57f233e810d17cb0783e1948c4e78fed792954a1
Nathaniel Fredericks <40307305+nathfreder@users.noreply.github.com>
2018-09-18 16:44:41 -0300

    Remove unneeded options for `css-loader`

--- webpack.config.js
+++ webpack.config.js
@@ -23,13 +23,6 @@ module.exports = {
           },
           {
             loader: 'css-loader',
-            options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: '[name]_[local]_[hash:base64]',
-              sourceMap: true,
-              minimize: true,
-            },
           },
         ],
       },
