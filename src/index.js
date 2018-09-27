070da95f4493be0236a5e202644463fd7cbabd10
Nathaniel Fredericks <40307305+nathfreder@users.noreply.github.com>
2018-09-27 18:05:01 -0300

    Add routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
-const App = () => <p>Hello World!</p>;
+import Home from './containers/Home';
+import About from './containers/About';
+
+const App = () => (
+  <Router>
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+    </>
+  </Router>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
